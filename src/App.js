d47ccc99232535c8a4cf4b3607f951fdfa3cedee
Billy <lebilly87@gmail.com>
2020-03-15 09:27:06 -0700

    added login with google component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import * as yup from 'yup';
 import { Machine } from 'xstate';
 import { useMachine } from '@xstate/react';
@@ -7,134 +7,182 @@ import { useForm } from 'react-hook-form';
 import './App.css';
 
 const tabsMachine = Machine({
-	id: 'tabs',
-	initial: 'login',
-	states: {
-		login: {
-			on: {
-				SIGN_UP: 'sign_up',
-				FORGOT_PASSWORD: 'forgot_password'
-			}
-		},
-		sign_up: {
-			on: {
-				LOGIN: 'login'
-			}
-		},
-		forgot_password: {
-			on: {
-				LOGIN: 'login',
-				SIGN_UP: 'sign_up'
-			}
-		}
-	}
+  id: 'tabs',
+  initial: 'login',
+  states: {
+    login: {
+      on: {
+        SIGN_UP: 'sign_up',
+        FORGOT_PASSWORD: 'forgot_password'
+      }
+    },
+    sign_up: {
+      on: {
+        LOGIN: 'login'
+      }
+    },
+    forgot_password: {
+      on: {
+        LOGIN: 'login',
+        SIGN_UP: 'sign_up'
+      }
+    }
+  }
 });
 
 const schema = yup.object().shape({
-	email: yup
-		.string()
-		.email()
-		.required(),
-	password: yup.string().min(8)
+  email: yup
+    .string()
+    .email()
+    .required(),
+  password: yup.string().min(8)
 });
 
 let renderCount = 0;
 
 function App() {
-	const [tab, send] = useMachine(tabsMachine);
-	const { register, handleSubmit, errors } = useForm({
-		validationSchema: schema
-	});
+  const [tab, send] = useMachine(tabsMachine);
+  const { register, handleSubmit, errors } = useForm({
+    validationSchema: schema
+  });
 
-	async function onSubmit(e) {
-		switch (tab.value) {
-			case 'login':
-				await fetch(`/api/login`, {
-					method: 'POST',
-					body: e
-				})
-					.then(res => res.json())
-					.then(console.log)
-					.catch(console.log);
-				break;
-			case 'sign_up':
-				await fetch(`/api/signup`, {
-					method: 'POST',
-					body: e
-				})
-					.then(res => res.json())
-					.then(console.log)
-					.catch(console.log);
-				break;
-			case 'forgot_password':
-				await fetch(`/api/forgot-password`, {
-					method: 'POST',
-					body: e
-				})
-					.then(res => res.json())
-					.then(console.log)
-					.catch(console.log);
-		}
-	}
+  // google api is made available before this declaration
+  const { gapi } = window;
 
-	renderCount++;
+  useEffect(() => {
+    if (gapi) {
+      gapi.signin2.render('my-signin2', {
+        scope: 'profile email',
+        width: 240,
+        height: 50,
+        longtitle: true,
+        theme: 'light',
+        onsuccess: onSuccess,
+        onfailure: onFailure
+      });
+    }
 
-	console.log(errors);
+    return () => {
+      if (gapi) {
+        const auth2 = gapi.auth2.getAuthInstance();
+        if (auth2) {
+          auth2.disconnect();
+        }
+      }
+    };
+  }, []);
 
-	return (
-		<div className='app'>
-			<form id='login-form' method='POST' onSubmit={handleSubmit(onSubmit)}>
-				<div className={`tabs`}>
-					<h2 onClick={() => send('LOGIN')}>
-						<div
-							style={{
-								width: tab.value === 'login' ? '100%' : 0
-							}}
-							className='overtop'
-						></div>
-						Login
-					</h2>
-					<h2 onClick={() => send('SIGN_UP')}>
-						<div
-							style={{
-								width: tab.value === 'sign_up' ? '100%' : 0
-							}}
-							className='overtop'
-						></div>
-						Sign Up
-					</h2>
-				</div>
-				<div className='control'>
-					<label htmlFor='email'>Email</label>
-					<input type='text' name='email' ref={register}></input>
-					{errors.email && <div className='error'>{errors.email.message}</div>}
-				</div>
-				{tab.value !== 'forgot_password' && (
-					<div className='control'>
-						<label htmlFor='password'>Password</label>
-						<input type='password' name='password' ref={register}></input>
-						{errors.password && (
-							<div className='error'>{errors.password.message}</div>
-						)}
-					</div>
-				)}
-				<div
-					style={{
-						display: 'flex',
-						justifyContent: 'flex-end'
-					}}
-				>
-					<button type='submit'>Submit</button>
-				</div>
-				{tab.value === 'login' ? (
-					<a href='#' onClick={() => send('FORGOT_PASSWORD')}>
-						Forgot My Password
-					</a>
-				) : null}
-			</form>
-			<div style={{ textAlign: 'center' }}>render count: {renderCount}</div>
-		</div>
-	);
+  async function onSubmit(e) {
+    switch (tab.value) {
+      case 'login':
+        await fetch(`/api/login`, {
+          method: 'POST',
+          body: e
+        })
+          .then(res => res.json())
+          .then(console.log)
+          .catch(console.log);
+        break;
+      case 'sign_up':
+        await fetch(`/api/signup`, {
+          method: 'POST',
+          body: e
+        })
+          .then(res => res.json())
+          .then(console.log)
+          .catch(console.log);
+        break;
+      case 'forgot_password':
+        await fetch(`/api/forgot-password`, {
+          method: 'POST',
+          body: e
+        })
+          .then(res => res.json())
+          .then(console.log)
+          .catch(console.log);
+        break;
+      default:
+        break;
+    }
+  }
+
+  function onSuccess() {
+    const auth2 = gapi.auth2.getAuthInstance();
+    if (auth2) {
+      auth2.disconnect();
+    }
+  }
+
+  function onFailure() {
+    const auth2 = gapi.auth2.getAuthInstance();
+    if (auth2) {
+      auth2.disconnect();
+    }
+  }
+
+  renderCount++;
+
+  return (
+    <div className='app'>
+      <form id='login-form' method='POST' onSubmit={handleSubmit(onSubmit)}>
+        <div className={`tabs`}>
+          <h2 onClick={() => send('LOGIN')}>
+            <div
+              style={{
+                width: tab.value === 'login' ? '100%' : 0
+              }}
+              className='overtop'
+            ></div>
+            Login
+          </h2>
+          <h2 onClick={() => send('SIGN_UP')}>
+            <div
+              style={{
+                width: tab.value === 'sign_up' ? '100%' : 0
+              }}
+              className='overtop'
+            ></div>
+            Sign Up
+          </h2>
+        </div>
+        <div className='control'>
+          <label htmlFor='email'>Email</label>
+          <input type='text' name='email' ref={register}></input>
+          {errors.email && <div className='error'>{errors.email.message}</div>}
+        </div>
+        {tab.value !== 'forgot_password' && (
+          <div className='control'>
+            <label htmlFor='password'>Password</label>
+            <input type='password' name='password' ref={register}></input>
+            {errors.password && <div className='error'>{errors.password.message}</div>}
+          </div>
+        )}
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'flex-end'
+          }}
+        >
+          <button type='submit'>Submit</button>
+        </div>
+        {tab.value === 'login' ? (
+          <div className='forgot-password' onClick={() => send('FORGOT_PASSWORD')}>
+            Forgot My Password
+          </div>
+        ) : null}
+        <div className='separator'>OR</div>
+        <ul className='oauth-list'>
+          {gapi && (
+            <li>
+              <div id='my-signin2'></div>
+            </li>
+          )}
+        </ul>
+      </form>
+
+      <div style={{ textAlign: 'center' }}>render count: {renderCount}</div>
+    </div>
+  );
 }
 
 export default App;
--- src/mirageServer/index.js
+++ src/mirageServer/index.js
@@ -1,69 +1,67 @@
 import { Server, Model, Response } from 'miragejs';
 import CryptoJS from 'crypto-js';
 
-var salt = CryptoJS.lib.WordArray.random(128 / 8);
+const salt = CryptoJS.lib.WordArray.random(128 / 8);
 
 function hashPassword(password) {
-	return CryptoJS.PBKDF2(password, salt).toString(CryptoJS.enc.Base64);
+  return CryptoJS.PBKDF2(password, salt).toString(CryptoJS.enc.Base64);
 }
 
 export default () =>
-	new Server({
-		models: {
-			user: Model
-		},
-		routes() {
-			this.namespace = '/api';
+  new Server({
+    models: {
+      user: Model
+    },
+    routes() {
+      this.namespace = '/api';
 
-			this.post('/login', (schema, request) => {
-				const { requestBody: body } = request;
-				const { email, password } = body;
-				const user = {
-					email,
-					password: hashPassword(password)
-				};
+      this.post('/login', (schema, request) => {
+        const { requestBody: body } = request;
+        const { email, password } = body;
+        const user = {
+          email,
+          password: hashPassword(password)
+        };
 
-				if (schema.users.findBy(user)) {
-					return new Response(200, {}, { message: 'user found' });
-				} else {
-					return new Response(400, {}, { error: 'user not found' });
-				}
-			});
+        if (schema.users.findBy(user)) {
+          return new Response(200, {}, { message: 'user found' });
+        } else {
+          return new Response(400, {}, { error: 'user not found' });
+        }
+      });
 
-			this.post('/signup', (schema, request) => {
-				const { requestBody: body } = request;
-				const { email, password } = body;
-				const user = {
-					email,
-					password: hashPassword(password)
-				};
-				const foundUser = schema.users.findBy(user);
-				if (!foundUser) {
-					schema.users.create(user);
-					return new Response(200, {}, { message: 'user created' });
-				} else {
-					return new Response(400, {}, { error: 'user already exists' });
-				}
-			});
+      this.post('/signup', (schema, request) => {
+        const { requestBody: body } = request;
+        const { email, password } = body;
+        const user = {
+          email,
+          password: hashPassword(password)
+        };
+        const foundUser = schema.users.findBy(user);
+        if (!foundUser) {
+          schema.users.create(user);
+          return new Response(200, {}, { message: 'user created' });
+        } else {
+          return new Response(400, {}, { error: 'user already exists' });
+        }
+      });
 
-			this.post('/forgot-password', (schema, request) => {
-				const { requestBody: body } = request;
-				const { email } = body;
-				const foundUser = schema.users.findBy({ email });
+      this.post('/forgot-password', (schema, request) => {
+        const { requestBody: body } = request;
+        const { email } = body;
 
-				// send success message regardless if user exists or not
-				if (!foundUser) {
-					return new Response(200, {}, { message: 'email sent' });
-				} else {
-					// send real email to user's email address
-					return new Response(200, {}, { message: 'email sent' });
-				}
-			});
-		},
-		seeds(server) {
-			server.create('user', {
-				email: 'user@test.com',
-				password: hashPassword('password')
-			});
-		}
-	});
+        const res = new Response(200, {}, { message: `email sent to ${email}` });
+        // send success message regardless if user exists or not
+        return res;
+      });
+
+      // Allow unhandled requests on the current domain to pass through
+      this.passthrough();
+    },
+    seeds(server) {
+      server.create('user', {
+        email: 'user@test.com',
+        password: hashPassword('password')
+      });
+    }
+  });
